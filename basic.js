baaa4f0707fc77e14ec8f7577591a72bb89408d4
Onwuzor Victor <victor.onwuzor@andela.com>
2020-02-06 09:45:55 +0100

    closure

--- basic.js
+++ basic.js
@@ -92,4 +92,87 @@ func(n) {
 }
 
 
+What is closure in JavaScript
+------------------------------
+Closure is an inner function that has access to the outer function variable scope and parameters.
+Closure has access to the outter function scope and global scope.
+
+func(name) {
+    const greetings = 'Hi';
+
+    // This is closure right here
+    // this inner function have access to the outter function variable greetings.
+    welcome() {
+        return `${greetings} ${name}`;
+    }
+
+    return welcome;
+}
+
+Note 1: Closure still have access to the outter function scope even after the outter function has been called.
+e.g
+// here, the outter fuction has been called and it has returned (it returned the inner function).
+const innerFunc = func('victor');
+innerFunc(); // Hi Victor
+
+Wait what? yeah!!!
+The outter function returned the inner function and assigned it to variable innerFunc
+But by the time we called innerFunc later, it still has access to greetings variable, name parameter
+
+Note 2: Closure store reference to the outter function variable not its actual value.
+So, even when the outter function variable has been changed before the closure is called
+
+e.g
+messages(name) {
+    let greetings = 'Hi';
+
+    // This is closure right here
+    // this inner function have access to the outter function variable greetings.
+    return {
+        genericGreeting: (word) => {
+            greetings = word;
+        },
+        welcomeMessage: () => {
+            return `${greetings} ${name}`;
+        }
+    }
+}
+
+// called outter function
+const greet = messages('Victor');
+greeting.welcomeMessage(); // Hi Victor
+
+// change outter function variable value
+greeting.genericGreeting('How are you');
+
+// call closure again
+greeting.welcomeMessage(); // How are you Victor
+
+
+Converting the memoization code into a closure
+-----------------------------------------------
+function func() {
+    // this is now private to the entire scope
+    const cache = {};
+
+    function innerFunc(n) {
+        return n * 10;
+    }
+
+    // closure
+    function memoize (n) {
+        if(n in cache) {
+            return cache[n];
+        } else {
+            // This process is memoization
+            // when get the result of the function, then cache it for later usage.
+            const result = innerFunc(n);
+            cache[n] = result;
+            return result;
+        }
+    }
+
+    return memoize;
+}
 */ 
+
