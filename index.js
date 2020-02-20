fb2ec5aa12651dbf8498cd71c2b34bbc25378e0a
Onwuzor Victor <victor.onwuzor@andela.com>
2020-02-20 07:51:40 +0100

    linear search algorithm

--- index.js
+++ index.js
@@ -77,3 +77,73 @@ const uniqueSort = (array) => {
 // const test = [3, 5, 5,5,10, 29, 1, 1, 3, 5];
 
 // console.log(uniqueSort(test));  [1,3,5,10,29]
+
+/*
+    Linear Search: This algorithm search for a value in an array by checking each value sequentially.
+    This algorithm is O(n) time complexity.
+    How it works: It loop through each value in the array and return the first value that match the search value
+    It takes to parameter; the list/array to search and the value to search for.
+*/
+
+function linearSearch(list, item) {
+    // loop through the list
+    // compare each value to the search item
+    // if they are match return ....
+    // else return ....
+    let index = -1;
+
+    // using For Loop
+    // for (listItem in list) {
+    //     if (list[listItem] === item) {
+    //         index = listItem;
+    //     } 
+    // }
+
+    // using forEach
+    list.forEach((listItem, i) => {
+        if (listItem === item) {
+            index = i;
+        }
+    });
+
+    return index;
+}
+
+
+function linearSearchRecursive(list, item) {
+    // loop through the list with recursion 
+    // by passing a start index 0 and the first element of index 0
+    // then subsequently increasing the index by 1 in each recursive call
+    // create a base case to check if the whole list item has been checked
+    // i.e compare the current index with the length - 1 of the array
+    // if the current index is equal to the length - 1 return the set index
+
+    let index = -1;
+
+    function recursion(i, listItem) {
+        // console.log('----->', i, listItem);
+        if (listItem === item) {
+            index = i;
+        }
+
+        if (i === list.length - 1) {
+            return index;
+        } else {
+            return recursion(i + 1, list[i + 1]);
+        }
+    }
+
+    recursion(0, list[0]);
+}
+
+// Test code 
+// const test = [2,6,7,90,100];
+
+// console.log(linearSearch(test, 90));  // 3
+
+/*
+    Binary Search: This algorithm search for a value in a SORTED array by cutting the side of the 
+    search area into have. It use the divide and conquer technique. It is only good for a sorted array.
+    This algorithm is Logarithmic O(logn) time complexity. 
+    How it works: It
+*/
