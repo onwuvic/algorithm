1e077e146f14d57143938a8c9820794eb3a30ec8
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-11-16 18:39:28 +0100

    understanding apply, call, bind js methods

--- basic.js
+++ basic.js
@@ -281,6 +281,100 @@ Where "this" is misunderstood (when "this" is not the value of the invoking obje
     where the THIS Method was defined.
 
 
+Understanding apply(), call(), and bind()
+-----------------------------------------
+They are methods on JavaScript function. JS functions are object under the hood therefore they have methods on them too.
+
+1. Bind: This method help us to explicitly set which object "this" should be bound to when a function or method is invoke.
+    Bind is not useful for assigning the THIS method to an object when borrowing a method, use apply() or call() instead.
+    Because using bind when borrowing method might override the method if it exist on object you are trying to bind the method to
+
+2. Call: This method help us to explicitly set which object "this" should be bound to when a function or method is invoke.
+    var avgScore = "global avgScore";
+
+    function avg(arrayOfScores) {
+        // Add all the scores and return the total
+        var sumOfScores = arrayOfScores.reduce (function (prev, cur, index, array) {
+            return prev + cur;
+        });
+
+        // The "this" keyword here will be bound to the global object, unless we set the "this" with Call or Apply
+        this.avgScore = sumOfScores / arrayOfScores.length;
+    }
+
+    var gameController = {
+        scores  :[20, 34, 55, 46, 77],
+        avgScore:null
+    }
+
+    // If we execute the avg function thus, "this" inside the function is bound to the global window object:
+    avg(gameController.scores);
+
+    console.log (window.avgScore); // 46.4
+    console.log (gameController.avgScore); // null
+
+    // reset the global avgScore
+    avgScore = "global avgScore";
+
+    // To set the "this" value explicitly, so that "this" is bound to the gameController,
+    // We use the call () method:
+    // it takes the object to point to and the parameter of the object
+    avg.call(gameController, gameController.scores);
+
+    console.log(window.avgScore); //global avgScore
+    console.log(gameController.avgScore); // 46.4
+
+
+    Difference between apply and call
+    The apply and call methods are almost identical when setting the this value except that you pass the function parameters to apply() as an array, 
+    while you have to list the parameters individually to pass them to the call() method.
+
+    Using Apply
+
+    var clientData = {
+        id: 094545,
+        fullName: "Not Set",
+        setUserName: function(firstName, lastName)  {
+            // this refers to the fullName property in this object
+            this.fullName = firstName + " " + lastName;
+        }
+    }
+
+    function getUserInput(firstName, lastName, callback, callbackObj) {
+        // The use of the Apply method below will set the "this" value to callbackObj
+        callback.apply(callbackObj, [firstName, lastName]);
+    }
+
+    Borrowing function:
+    The most common use for the Apply and Call methods in JavaScript is probably to borrow functions.
+
+    var gameController = {
+        scores  :[20, 34, 55, 46, 77],
+        avgScore:null,
+        players :[
+            {name:"Tommy", playerID:987, age:23},
+            {name:"Pau", playerID:87, age:33}
+        ]
+    }
+
+    var appController = {
+        scores: [900, 845, 809, 950],
+        avgScore: null,
+        avg: function () {
+            var sumOfScores = this.scores.reduce (function (prev, cur, index, array) {
+                return prev + cur;
+            });
+
+            this.avgScore = sumOfScores / this.scores.length;
+        }
+    }
+
+    appController.avg.apply(gameController);
+    console.log (gameController.avgScore); // 46.4
+
+    // appController.avgScore is still null; it was not updated, only gameController.avgScore was updated
+    console.log (appController.avgScore); // null
+
 JavaScript the Hard Part
 -------------------------
 1. Principles of JavaScript
